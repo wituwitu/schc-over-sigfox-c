a30ce0fba78bc1b9ad0b62760306b921c094ab72
Diego Wistuba <wistuba@niclabs.cl>
2023-01-30 12:59:50 -0300

    Added global variables in config

---
+++ config/schc.c
@@ -0,0 +1,24 @@
+//
+// Created by witu on 1/30/23.
+//
+
+#include "schc.h"
+
+char FR_MODE[] = "ACK ON ERROR";
+int RETRANSMISSION_TIMEOUT = 100;
+int SIGFOX_DL_TIMEOUT = 60; /* in seconds */
+int INACTIVITY_TIMEOUT = 500;  /* in seconds */
+int MAX_ACK_REQUESTS = 5;
+int L2_WORD_SIZE= 8;
+int DELAY_BETWEEN_FRAGMENTS = 10; /* in seconds */
+
+/* Sender config */
+int ENABLE_MAX_ACK_REQUESTS = 1;
+
+/* Receiver config */
+int RESET_DATA_AFTER_REASSEMBLY = 0 ;
+int CHECK_FOR_CALLBACK_RETRIES  = 1;
+int DISABLE_INACTIVITY_TIMEOUT = 0;
+
+char RECEIVER_URL[] = "http://localhost:5000/receive";
+
---
+++ config/schc.h
@@ -0,0 +1,25 @@
+//
+// Created by witu on 1/30/23.
+//
+
+#ifndef SCHC_OVER_SIGFOX_C_SCHC_H
+#define SCHC_OVER_SIGFOX_C_SCHC_H
+
+#endif //SCHC_OVER_SIGFOX_C_SCHC_H
+
+extern char FR_MODE[];
+extern int RETRANSMISSION_TIMEOUT;
+extern int SIGFOX_DL_TIMEOUT;
+extern int INACTIVITY_TIMEOUT;
+extern int MAX_ACK_REQUESTS;
+extern int L2_WORD_SIZE;
+extern int DELAY_BETWEEN_FRAGMENTS;
+
+// Sender config
+extern int ENABLE_MAX_ACK_REQUESTS;
+
+// Receiver config
+extern int RESET_DATA_AFTER_REASSEMBLY;
+extern int CHECK_FOR_CALLBACK_RETRIES;
+extern int DISABLE_INACTIVITY_TIMEOUT;
+extern char RECEIVER_URL[];
