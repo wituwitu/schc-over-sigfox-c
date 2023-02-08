434eba1923295b511a1ff1e2f90ba290e59188af
Diego <witu@witu.cl>
2023-02-08 19:17:22 -0300

    Creating storage modules

---
+++ db/storage.c
@@ -0,0 +1,5 @@
+//
+// Created by witu on 08-02-23.
+//
+
+#include "storage.h"
---
+++ db/storage.h
@@ -0,0 +1,8 @@
+//
+// Created by witu on 08-02-23.
+//
+
+#ifndef SCHC_OVER_SIGFOX_C_STORAGE_H
+#define SCHC_OVER_SIGFOX_C_STORAGE_H
+
+#endif //SCHC_OVER_SIGFOX_C_STORAGE_H
