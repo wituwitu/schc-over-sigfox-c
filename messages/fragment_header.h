4d980471b135a8104cefe49d0de873b0e92a1c50
Diego Wistuba <wistuba@niclabs.cl>
2023-02-15 20:21:51 -0300

    creating fragment header...

---
+++ messages/fragment_header.h
@@ -0,0 +1,8 @@
+#include "rule.h"
+#ifndef SCHC_OVER_SIGFOX_C_FRAGMENT_HEADER_H
+#define SCHC_OVER_SIGFOX_C_FRAGMENT_HEADER_H
+
+#endif // SCHC_OVER_SIGFOX_C_FRAGMENT_HEADER_H
+
+typedef struct {
+} FragmentHeader;
